bba118bbd0158ba2ead00e229daee545aa138446
Sjsan69 <sankalp001jain@gmail.com>
2023-11-17 22:49:00 +0530

    Main Included

---
+++ script.js
@@ -0,0 +1,37 @@
+function showSuggestions() {
+    var input = document.getElementById('searchInput');
+    var suggestionList = document.getElementById('suggestionList');
+    var inputValue = input.value.trim();
+
+    // Clear previous suggestions
+    suggestionList.innerHTML = '';
+
+    // Show suggestions only if there's a search query
+    if (inputValue !== '') {
+        // In a real implementation, you would fetch suggestions from a backend
+        // For simplicity, let's use a hardcoded array
+        var suggestions = ['HTML', 'CSS', 'JavaScript', 'Web Development', 'Programming'];
+
+        // Display suggestions
+        suggestions.forEach(function (suggestion) {
+            var li = document.createElement('li');
+            li.textContent = suggestion;
+            suggestionList.appendChild(li);
+        });
+    }
+}
+
+function performSearch() {
+    var searchInput = document.getElementById('searchInput');
+    var query = searchInput.value.trim();
+
+    // In a real implementation, you would redirect to a search results page
+    // For simplicity, let's log the query to the console
+    console.log('Performing search for:', query);
+}
+
+function luckySearch() {
+    // In a real implementation, you would redirect to a random search result page
+    // For simplicity, let's log a message to the console
+    console.log('Feeling lucky!');
+}
